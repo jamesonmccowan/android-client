ac91b93cb61ee410c80775c50c051ed22fa8229e
Minh Vu <vnm241191@gmail.com>
2014-03-02 17:38:23 -0800

    Changing Maps.java

--- RouteTracker/src/main/java/com/pedalportland/routetracker/Maps.java
+++ RouteTracker/src/main/java/com/pedalportland/routetracker/Maps.java
@@ -1,6 +1,3 @@
-/**
- * Created by Minh Vu on 2/20/14.
- */
 package com.pedalportland.routetracker;
 
 import android.app.Activity;
@@ -11,71 +8,71 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import com.google.android.gms.maps.model.PolylineOptions;
-import com.pedalportland.routetracker.R;
 
 import java.util.Arrays;
-
-
 /**
- * Created by Minh Vu on 2/18/14.
+ * Created by Minh Vu on 2/27/14.
  */
 public class Maps extends Activity {
-
-    Double[] position = {45.49216607, -122.79390729, 45.49212721, -122.79416967,
-            45.49209675, -122.79431325, 45.49201231, -122.79448298,
-            45.49242192, -122.79465708, 45.4927973, -122.79473063};
-    LatLng PORTLANDSU = new LatLng(45.49216607, -122.79390729);
-    LatLng PORTLANDSQ = new LatLng(45.49212721, -122.79416967);
-
-    int test = 0;
-    LatLng[] point = new LatLng[2];
-
     GoogleMap map;
+    LatLngBounds.Builder builder = new LatLngBounds.Builder();
 
-    @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_maps);
+
+        LatLng trip[] = {new LatLng(45.51355928,-122.68487634),
+                new LatLng(45.51354346,-122.68515974),
+                new LatLng(45.51349448,-122.68542416),
+                new LatLng(45.51319437,-122.68564974),
+                new LatLng(45.51266601,-122.68383533),
+                new LatLng(45.51231659,-122.68241821),
+                new LatLng(45.51225868,-122.68231447),
+                new LatLng(45.51208805,-122.68217059),
+                new LatLng(45.51201439,-122.68208911),
+                new LatLng(45.511911370,-122.68197378),
+                new LatLng(45.51324567,-122.68219067),
+                new LatLng(45.51346769,-122.68456789),
+                new LatLng(45.51201598,-122.68575123),
+                new LatLng(45.51689784,-122.68123456)};
+
+        LatLng PORTLANDSQ = new LatLng(45.51355928,-122.68487634);
+        LatLng PORTLANDSU = new LatLng(45.511911370,-122.68197378);
 
         map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
                 .getMap();
 
-        Marker PortlandSU = map.addMarker(new MarkerOptions().position(PORTLANDSU)
+        map.addMarker(new MarkerOptions().position(PORTLANDSU)
                 .title("Portland State University,OR"));
-        Marker PioneerSQ = map.addMarker(new MarkerOptions()
-                .position(PORTLANDSQ)
-                .title("PioneerSQ")
-                .snippet("Heart of portland")
-                .icon(BitmapDescriptorFactory
+
+        map.addMarker(new MarkerOptions().position(PORTLANDSQ).title("PioneerSQ")
+                .snippet("Heart of portland").icon(BitmapDescriptorFactory
                         .fromResource(R.drawable.ic_launcher)));
 
-        for(int i = 0, y = 1, k = 0; k <= (position.length -3); i+=2, y+=2, k+=1)
+        map.addPolyline(new PolylineOptions().addAll(Arrays.asList(trip)).width(6).
+                color(Color.BLUE).geodesic(true));
+
+        for (int i = 0; i <=trip.length -1; i++)
         {
-            if (test == 0)
-            {
-                LatLng a = new LatLng(position[i], position[y]);
-                point[0] = a;
-                test++;
-            }
-            else if (test == 1)
-            {
-                LatLng b = new LatLng(position[i], position[y]);
-                point[1] = b;
-                test = 0;
-                map.addPolyline(new PolylineOptions().add(point[0], point[1]).width(2).
-                        color(Color.BLUE).geodesic(true));
-                Arrays.fill(point, null);
-                i-=2;
-                y-=2;
-            }
+            builder.include(trip[i]);
         }
-        // Zoom in, animating the camera.
-        map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+
+        Arrays.fill(trip, null);
+
+        map.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
+            @Override
+            public void onCameraChange(CameraPosition arg0) {
+                // Move camera.
+                map.moveCamera(CameraUpdateFactory.newLatLngBounds(builder.build(), 10));
+                // Remove listener to prevent position reset on camera move.
+                map.setOnCameraChangeListener(null);
+            }
+        });
     }
 }
